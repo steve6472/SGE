7a806e0eb2646974835730c51546690374bcdb9c
steve6472 <steve64959@gmail.com>
2017-10-24 11:35:56 +0200

    Create SpriteUtils.java

---
+++ src/com/steve6472/sge/gfx/SpriteUtils.java
@@ -0,0 +1,24 @@
+package com.steve6472.sge.gfx;
+
+import static com.steve6472.sge.main.Util.printf;
+import static com.steve6472.sge.main.Util.printfd;
+import com.steve6472.sge.main.IUtil;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
+import com.steve6472.sge.main.SGEMain;
+
+public class SpriteUtils implements IUtil
+{
+  
+}
